80fdd58ad09efe9c9cfd3ec326db936f38a1e627
D <pythonmainer@gmail.com>
2019-08-07 22:46:33 +0300

    тесты для добавления и удаления проектов в Mantis3

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -3,8 +3,16 @@
 from model.project import Project
 
 
+# def test_add_project(app, db, json_project):
+#     project = json_project
+#     old_list = db.get_project_list()
+#     app.project.add_new_project(project)
+#     new_list = db.get_project_list()
+#     assert len(old_list) + 1 == len(new_list)
+#     old_list.append(project)
+#     assert sorted(old_list, key=Project.name_key) == sorted(new_list, key=Project.name_key)
 
-def test_add_project(app, db, json_project):
+def test_new_add_project(app, db, json_project):
     project = json_project
     old_list = db.get_project_list()
     app.project.add_new_project(project)
